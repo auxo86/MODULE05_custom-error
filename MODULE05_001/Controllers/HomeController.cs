6c4554831618e4879ca1aa439bbdeee3624155fc
auxo86 <auxo86@gmail.com>
2017-05-18 21:32:22 +0800

    Add 404自訂錯誤的View

---
+++ MODULE05_001/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MODULE05_001.Controllers
+{
+    //[HandleError(View = "AA")] 
+    public class HomeController : Controller
+    {
+        // GET: Home ^^
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Web.config ==> <customErrors mode="On"></customErrors>
+        //[HandleError]
+        public ActionResult ExceptionDemo()
+        {
+            int i = 0;
+            int j = 10 / i;
+
+            return View();
+        }
+    }
+}
